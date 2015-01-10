3e4b5f5687ae1188a720efac667e701fbb74cf78
Mohammad Anwari <mdamt@mdamt.net>
2015-01-10 08:45:01 +0700

    Change target

--- F-Droid/ant-prepare.sh
+++ F-Droid/ant-prepare.sh
@@ -7,23 +7,23 @@ place_support_v4() {
     cp libs/android-support-v4.jar $1/libs/
 }
 
-android update lib-project --path $EXTERN/UniversalImageLoader/library
+android update lib-project --path $EXTERN/UniversalImageLoader/library --target android-21
 
-android update lib-project --path $EXTERN/AndroidPinning
+android update lib-project --path $EXTERN/AndroidPinning --target android-21
 
-android update lib-project --path $EXTERN/MemorizingTrustManager
+android update lib-project --path $EXTERN/MemorizingTrustManager --target android-21
 
-android update lib-project --path $EXTERN/libsuperuser/libsuperuser
+android update lib-project --path $EXTERN/libsuperuser/libsuperuser --target android-21
 
-android update lib-project --path $EXTERN/zxing-core
+android update lib-project --path $EXTERN/zxing-core --target android-21
 
-android update lib-project --path $EXTERN/android-support-v4-preferencefragment
+android update lib-project --path $EXTERN/android-support-v4-preferencefragment --target android-21
 place_support_v4 $EXTERN/android-support-v4-preferencefragment
 
-android update lib-project --path $EXTERN/Support/v7/appcompat --target android-19
+android update lib-project --path $EXTERN/Support/v7/appcompat --target android-21
 place_support_v4 $EXTERN/Support/v7/appcompat
 
-android update project --path . --name F-Droid
+android update project --path . --name F-Droid --target android-21
 
 { echo -e "\nSuccessfully updated the main project.\n"; } 2>/dev/null
 
